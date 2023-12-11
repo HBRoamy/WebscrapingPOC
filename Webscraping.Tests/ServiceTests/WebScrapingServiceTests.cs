65b87e236c91400c48fe93bbe144a9a7cdb9a718
Harshit Bhardwaj <harshit.bhardwaj@nagarro.com>
2023-12-11 13:47:21 +0530

    refactored UTs, added backup files

--- Webscraping.Tests/ServiceTests/WebScrapingServiceTests.cs
+++ Webscraping.Tests/ServiceTests/WebScrapingServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Moq;
+using System.Net;
 using WebScraping.Services;
 using WebScraping.Services.Interfaces;
 
@@ -13,8 +14,40 @@ public class WebScrapingServiceTests
         _mockHttpService = new Mock<IHttpClientService>();
     }
 
+    [Theory]
+    [MemberData(nameof(ServiceTestsData))]
+    public async Task GrabResourceFromURI_ReturnsValidString_WithDifferentApiResponses(string resourcePath, HttpStatusCode statusCode, string? expectedValue)
+    {
+        //Arrange
+        _mockHttpService.Setup(x => x.GetAsync(It.IsAny<string>()))
+            .ReturnsAsync(
+                new HttpResponseMessage
+                {
+                    StatusCode = statusCode,
+                    Content = statusCode is HttpStatusCode.OK ? new StringContent(expectedValue!) : null
+                }
+            );
+
+        //Act
+        var service = new WebScrapingService(_mockHttpService.Object);
+        string result = await service.GrabResourceFromURIOrDefaultAsync(string.Empty, resourcePath);
+
+        //Assert
+        result.Should().NotBeNullOrEmpty();
+        result.Should().Be(expectedValue);
+    }
+
+    public static IEnumerable<object[]> ServiceTestsData()
+    {
+        yield return new object[] { "Resources/testCarsData.json", HttpStatusCode.OK, File.ReadAllText("Resources/testCarsData.json") }; // Successful API Response
+        yield return new object[] { "Resources/testHTMLData.txt", HttpStatusCode.NotFound, File.ReadAllText("Resources/testHTMLData.txt") }; // Unsuccesful API Response
+    }
+
+    /*
+    Seperate unit tests for both scenarios
+
     [Fact]
-    public async Task Test_GrabResourceFromURI_ReturnsValidString_WithSuccessfulApiResponse()
+    public async Task GrabResourceFromURI_ReturnsValidString_WithSuccessfulApiResponse()
     {
         //Arrange
         string expectedValue = File.ReadAllText("Resources/testCarsData.json");
@@ -27,7 +60,7 @@ public class WebScrapingServiceTests
             );
 
         //Act
-        var service = new WebScrapingService( _mockHttpService.Object );
+        var service = new WebScrapingService(_mockHttpService.Object);
         string result = await service.GrabResourceFromURIOrDefaultAsync(string.Empty, "Resources/testCarsData.json");
 
         //Assert
@@ -36,7 +69,7 @@ public class WebScrapingServiceTests
     }
 
     [Fact]
-    public async Task Test_GrabResourceFromURI_ReturnsValidString_WithUnsuccessfulApiResponse()
+    public async Task GrabResourceFromURI_ReturnsValidString_WithUnsuccessfulApiResponse()
     {
         //Arrange
         string expectedValue = File.ReadAllText("Resources/testHTMLData.txt");
@@ -55,4 +88,5 @@ public class WebScrapingServiceTests
         result.Should().NotBeNullOrEmpty();
         result.Should().Be(expectedValue);
     }
+    */
 }
